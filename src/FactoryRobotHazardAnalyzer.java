dffa549d76a3deac44de8dfec540b7e1c74577b2
adithya <adithyasivaraman5@gmail.com>
2026-02-02 14:38:21 +0530

    [Adithya] Added : UC1 : Print Factory Robot Hazard Analyzer

---
+++ src/FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,16 @@
+/**
+ * Factory Robot Hazard Analyzer
+ *
+ * UC1: Display Static Hazard Message
+ *
+ * Prints the system name to the console.
+ *
+ * @author Adithya
+ * @version 1.0
+ */
+public class FactoryRobotHazardAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("Factory Robot Hazard Analyzer");
+    }
+}
