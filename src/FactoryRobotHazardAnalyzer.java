53ff11cd40dc1cfe0c0e8bedb143d7bf9fa1dfde
adithya <adithyasivaraman5@gmail.com>
2026-02-02 15:03:22 +0530

    [Adithya] Added : UC7 : Introduced RobotHazardAuditor Class

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 /**
  * Factory Robot Hazard Analyzer
  *
- * UC6: Custom Exception Handling
+ * UC7: Uses RobotHazardAuditor class.
  *
  * @author Adithya
- * @version 6.0
+ * @version 7.0
  */
 public class FactoryRobotHazardAnalyzer {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        RobotHazardAuditor auditor = new RobotHazardAuditor();
 
         System.out.println("Factory Robot Hazard Analyzer");
 
@@ -27,7 +28,7 @@ public class FactoryRobotHazardAnalyzer {
             System.out.println("Enter Machinery State (Worn/Faulty/Critical):");
             String machineryState = scanner.nextLine();
 
-            double risk = calculateHazardRisk(
+            double risk = auditor.calculateHazardRisk(
                     armPrecision,
                     workerDensity,
                     machineryState
@@ -38,41 +39,5 @@ public class FactoryRobotHazardAnalyzer {
         } catch (RobotSafetyException e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    public static double calculateHazardRisk(
-            double armPrecision,
-            int workerDensity,
-            String machineryState)
-            throws RobotSafetyException {
-
-        if (armPrecision < 0.0 || armPrecision > 1.0) {
-            throw new RobotSafetyException(
-                    "Error: Arm precision must be 0.0-1.0"
-            );
-        }
-
-        if (workerDensity < 1 || workerDensity > 20) {
-            throw new RobotSafetyException(
-                    "Error: Worker density must be 1-20"
-            );
-        }
-
-        double machineRiskFactor;
-
-        if (machineryState.equals("Worn")) {
-            machineRiskFactor = 1.3;
-        } else if (machineryState.equals("Faulty")) {
-            machineRiskFactor = 2.0;
-        } else if (machineryState.equals("Critical")) {
-            machineRiskFactor = 3.0;
-        } else {
-            throw new RobotSafetyException(
-                    "Error: Unsupported machinery state"
-            );
-        }
-
-        return ((1.0 - armPrecision) * 15.0)
-                + (workerDensity * machineRiskFactor);
     }
 }
